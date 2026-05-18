1e3eefa9083b7e9ea3bbb9c1d229320639b5ecc3
Gabriel Stachnik <63073744+Celiek@users.noreply.github.com>
2026-05-18 22:26:19 +0200

    Service do autoryzacji użytkowników

---
+++ security-service/src/main/java/com/ksef/security_service/DTO/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.ksef.security_service.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+}
---
+++ security-service/src/main/java/com/ksef/security_service/config/KeycloackConfig.java
@@ -0,0 +1,25 @@
+package com.ksef.security_service.config;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloackConfig {
+
+    @Bean
+    public Keycloak keycloak(){
+        return KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8085")
+                .realm("ksef")
+                .clientId("admin-cli")
+                .username("celiek")
+                .password("admin")
+                .grantType(OAuth2Constants.PASSWORD)
+                .build();
+    }
+
+
+}
